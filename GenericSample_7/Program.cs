0389cd65fc1f2a7e1317af20fb001708103704d6
Ian <rutsuki123@gmail.com>
2019-11-24 19:38:59 +0800

    add GenericSample_7

---
+++ GenericSample_7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GenericSample_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass = new MyClass();
+            myClass.Add<string>("Test");
+
+            myClass.Add<string, int>("Test", 123);
+        }
+    }
+
+    class MyClass
+    {
+        public void Add<TValue>(TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add<TValue1, TValue2>(TValue1 value1, TValue2 value2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
