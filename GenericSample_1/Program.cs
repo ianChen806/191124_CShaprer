c57e220d27bcd41a5ce322fc6818844a087b9858
Ian <rutsuki123@gmail.com>
2019-11-24 11:21:37 +0800

    add sample_1

---
+++ GenericSample_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace GenericSample_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list1 = new List<string>();
+            list1.Add("123");
+
+            var list2 = new List<int>();
+            list2.Add(1);
+
+            var list3 = new List<MyCalss>();
+            list3.Add(new MyCalss());
+        }
+    }
+
+    internal class MyCalss
+    {
+    }
+}
