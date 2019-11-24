6af6d0c0e39aa830911f30cd88c4cb80957f6838
Ian <rutsuki123@gmail.com>
2019-11-24 22:13:15 +0800

    add ActionSample_1

---
+++ ActionSample_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ActionSample_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action action = () =>
+            {
+                Console.WriteLine("123");
+            };
+            action.Invoke();
+
+            Action<string> action2 = value =>
+            {
+                Console.WriteLine(value);
+            };
+            action2("test");
+        }
+    }
+}
