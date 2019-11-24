eb3ac0ac09d7fddf8118b081c6c5ebf6f79d7478
Ian <rutsuki123@gmail.com>
2019-11-24 20:01:06 +0800

    add GenericSample_9

---
+++ GenericSample_10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GenericSample_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var genericFactory = new GenericFactory();
+            var instance = genericFactory.Instance<MyClass>();
+            instance.Test();
+        }
+    }
+
+    internal class MyClass
+    {
+        public void Test()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+
+    internal class GenericFactory
+    {
+        public TType Instance<TType>()
+        {
+            return Activator.CreateInstance<TType>();
+        }
+    }
+}
