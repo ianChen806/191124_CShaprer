826860c4786de216a05af4f11a0b4b1110192ded
Ian <rutsuki123@gmail.com>
2019-11-24 21:59:19 +0800

    add FuncSample_2

---
+++ FuncSample_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FuncSample_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int> action = Test;
+            
+            var result = action();
+            
+            Console.WriteLine(result);
+        }
+
+        private static int Test()
+        {
+            return 1;
+        }
+    }
+}
