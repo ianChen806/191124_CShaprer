e2bbdc759f2e12a48eec3ce3450e5ddf482b470a
Ian <rutsuki123@gmail.com>
2019-11-24 12:07:33 +0800

    add GenericSample_4

---
+++ GenericSample_4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+
+namespace GenericSample_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrayList = new ArrayList();
+            arrayList.Add("1");
+            arrayList.Add(1);
+            arrayList.Add(new object());
+        }
+    }
+}
