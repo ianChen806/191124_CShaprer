2b9edae923988051636450f73d934487775373be
Ian <rutsuki123@gmail.com>
2019-11-24 21:49:37 +0800

    add GenericSample_12

---
+++ GenericSample_12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace GenericSample_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myClass = new MyClass();
+            var value = myClass.GetIndex<MyEnum>(MyEnum.D);
+
+            Console.WriteLine(value);
+        }
+    }
+
+    enum MyEnum
+    {
+        A,
+        B,
+        D,
+    }
+
+    internal class MyClass
+    {
+        public int GetIndex<TEnum>(TEnum value)
+            where TEnum : Enum
+        {
+            var list = Enum.GetValues(typeof(TEnum)).Cast<TEnum>().ToList();
+            return list.IndexOf(value);
+        }
+    }
+}
